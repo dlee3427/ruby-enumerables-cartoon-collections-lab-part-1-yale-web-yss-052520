5ad756a73fe3e91497d58ba8be88d004ab24df31
David Lee <david.lee.sl2634@yale.edu>
2020-05-16 22:21:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |character, index|
-    p "#{index + 1}. #{character}" 
+    p "#{index.to_i + 1}. #{character}" 
   end
 end
